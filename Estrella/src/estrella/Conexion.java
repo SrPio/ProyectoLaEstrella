c4e54030506fce298560dd9551e59be9848633b5
Charlie991221 <carlos.jimenez.castrillon@correounivalle.edu.co>
2021-03-19 11:23:24 -0500

    Cambios en conexion.java

--- Estrella/src/estrella/Conexion.java
+++ Estrella/src/estrella/Conexion.java
@@ -6,22 +6,26 @@ import java.sql.DriverManager;
 
 public class Conexion {
     
-    public static final String URL = "jdbc:postgresql://localhost:5432/LaEstrella"; //Direccion, puerto y nombre de la Base de Datos
-    public static final String USERNAME = "postgres"; //Usuario de Acceso a MySQL
-    public static final String PASSWORD = "jaramillo1999"; //Password del usuario
-    
-     public static Connection getConection() {
-        Connection con = null;
+  public static final String URL = "jdbc:postgresql://localhost:5432/LaEstrella";
+  public static final String USERNAME = "postgres";
+  public static final String PASSWORD = "jaramillo1999";
+  
+  /*
+   *Esto es la funcion para obtener la conexion a la base de datos
+   */
+  public static Connection getConection(){
+      
+    Connection con = null;
 
-        try {
+    try {
 
-            Class.forName("org.postgresql.Driver");
-            con = (Connection) DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Conexion a base de datos exitosa!");
+      Class.forName("org.postgresql.Driver");
+      con = (Connection) DriverManager.getConnection(URL, USERNAME, PASSWORD);
+      System.out.println("Conexion a base de datos exitosa!");
 
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return con;
+    } catch (Exception e) {
+      System.out.println(e);
     }
+    return con;
+  }
 }
